289abb29115a7b6b5007d5770305b1d0554ad3bc
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-04-22 00:39:36 +0000

    set-up for solution

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ l1.next.next.next.next.next = new ListNode(15)
 // 1 => 5 => 7 => 10 => 12 => 15 => null
 //1
 //10
-console.log(l1)
+// console.dir(l1)
 
 ////   1 =>       5 =>     7 => 10 =>      12 =>       15 => null
 // start list
@@ -40,18 +40,73 @@ save next after found value for middle and end list
 
 point to null if no end List
 */
-
-function swap (l1, target1, target2) {
-  let current = li;
+const findVal = () =>{
+  
+}
+function swap (list1, target1, target2) {
+  let current = list1;
   let head = current;
-  let start;
-  let middle;
-  let end;
+  let firstNode = null
+  let secondNode = null
+  
+  // let start = new ListNode(0);
+  // let startHead = start
+  // let middle = null;
+  // let middleHead = middle
+  // let end = null;
+  // let endHead = end
   
   while(current !== null){
+    // let next = current.next
+    // if (current.val !== target1 && current.val !== target2 && !middle && !end){
+    //   start.next = current
+    //   start = start.next
+    // }
     
-    current.next;
+    if(current.val === target1){
+       firstNode = current;
+       // middle = current.next
+       // // middle = middle.next
+      // current = current.next
+      // continue
+    }
+    
+    // if (current.val === target1 && current.val !== target2 && middle){
+    //   middle.next = current
+    //   middle = middle.next
+    // }
+
+    if(current.val === target2) {
+      secondNode = current;
+      // end = current.next;
+      // current = current.next
+      // continue
+    }
+
+    current = current.next;
   }
+
+  while (current !== null){
+     if(current.val === target1){
+       // swap second
+     }
+     if(current.val === target2){
+       // swap first
+     }
+
+    current = current.next
+  }
+
+////  start {1 =>   5 =>  7 => }     10 =>      12 =>       15 => null
+  // start.next = secondNode
+  // secondNode.next = middle
+  
+// console.log(firstNode, secondNode)
+  console.log('Start', start)
+  console.log('middle', middle)
+  console.log('end', end)
+  
+  return 'hi'
 }
 
 
